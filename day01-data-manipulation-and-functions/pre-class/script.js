f8324e910ea4e0174bf7c2aae44cf012c5e75534
dcgy <88198197+dcgy@users.noreply.github.com>
2022-03-22 21:47:45 +0800

    Functions 1 Mod [Complete]

--- day01-data-manipulation-and-functions/pre-class/script.js
+++ day01-data-manipulation-and-functions/pre-class/script.js
@@ -26,12 +26,26 @@ var functionsExampleMain = function (input) {
 
 var trainSpeedMain = function (input) {
   // Attempt the Comfortable: Train Speed exercise from the Functions I module below with trainSpeedMain as the main function.
-  var myOutputValue = "hello world";
+  let totalDelay = input;
+  let totalTime = 2;
+  const totalDistance = 400;
+  let timeRequired = totalTime - totalDelay;
+  let speed = totalDistance / timeRequired;
+  var myOutputValue = speed;
   return myOutputValue;
+};
+
+var currentHourAngle = function (hour) {
+  let hourAngle = (hour / 12) * 360;
+  console.log(hourAngle);
+
+  return hourAngle;
 };
 
 var clockMain = function (input) {
   // Attempt the More Comfortable: Clock exercise from the Functions I module below with clockMain as the main function.
-  var myOutputValue = "hello world";
+  let hourAngle = currentHourAngle(1);
+  let minuteAngle = (input / 60) * 360;
+  var myOutputValue = minuteAngle - hourAngle;
   return myOutputValue;
 };
